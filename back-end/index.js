6d9284380e9141df36370df6b1f4ea14c79d00f9
IDemidova <irina.demidova.irina@gmail.com>
2022-08-17 18:14:42 +0300

    feat: add db name and listeners for start and error

--- back-end/index.js
+++ back-end/index.js
@@ -1,13 +1,21 @@
 const PORT = 3000;
-const CONNECT = 'mongodb://127.0.0.1:27017';
+const CONNECT = 'mongodb://127.0.0.1:27017/RSClone';
 const express = require('express');
 const mongoose = require('mongoose');
+const db = mongoose.connection;
 const routerV1 = require('./routers/v1');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', routerV1);
+
+db.on('eror', (error) => {
+  console.log(error);
+});
+db.on('open', () => {
+  console.log('Connected to DB.');
+});
 
 const start = async () => {
   try {
