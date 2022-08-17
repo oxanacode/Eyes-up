92a85846a7ab1581885a1e59d79a2d42e49e9ebd
IDemidova <irina.demidova.irina@gmail.com>
2022-08-17 17:21:28 +0300

    feat: add router for api v1

--- back-end/index.js
+++ back-end/index.js
@@ -2,10 +2,12 @@ const PORT = 3000;
 const CONNECT = 'mongodb://127.0.0.1:27017';
 const express = require('express');
 const mongoose = require('mongoose');
+const routerV1 = require('./routers/v1');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', routerV1);
 
 const start = async () => {
   try {
