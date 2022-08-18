0270088cb5bb61745cd6e6461bf2cc980e944d96
IDemidova <irina.demidova.irina@gmail.com>
2022-08-18 18:02:53 +0300

    refactor: remove depricated params from lessons search

--- back-end/controllers/v1/lessons.js
+++ back-end/controllers/v1/lessons.js
@@ -5,18 +5,9 @@ exports.getLessons = async (req, res) => {
     const params = req.query.complexity
       ? { language: req.query.language, complexity: req.query.complexity }
       : { language: req.query.language };
-    let data;
+    const dataToFind = await lessonModel.find(params);
 
-    if (req.query.page && req.query.limit) {
-      const page = req.query.page;
-      const limit = req.query.limit;
-      const dataToSkip = page - 1 * limit;
-      data = await lessonModel.find(params).skip(dataToSkip).limit(limit);
-    } else {
-      data = await lessonModel.find(params).count();
-    }
-
-    res.json(data);
+    res.json(dataToFind);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
