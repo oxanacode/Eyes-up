4d9fe69aea845ed48e4f6c3a79f4b2a5d49ccafb
IDemidova <irina.demidova.irina@gmail.com>
2022-08-19 16:18:24 +0300

    feat: add current user field for state

--- front-end/src/scripts/state.ts
+++ front-end/src/scripts/state.ts
@@ -1,6 +1,16 @@
 import { Lang, Page, Theme } from '../types/enums';
+import { CurrentUser } from '../types/types';
 
 class State {
+  public static noLogin = '';
+
+  public static noAvatar = 0;
+
+  public static currentUser: CurrentUser = {
+    login: State.noLogin,
+    avatar: State.noAvatar,
+  };
+
   public static currentPage = Page.home;
 
   public static currentLang = Lang.en;
