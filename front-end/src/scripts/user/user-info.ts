db92562b2cef471b148168a87dbeecb3f3898b62
IDemidova <irina.demidova.irina@gmail.com>
2022-08-27 15:35:26 +0300

    refactor: change put to patch method for updating user and lesson

--- front-end/src/scripts/user/user-info.ts
+++ front-end/src/scripts/user/user-info.ts
@@ -1,16 +1,17 @@
 import { UserData } from '../../types/types';
+import { Data } from '../../types/enums';
 
 class UserInfo implements UserData {
   constructor(
     public login: string,
     public password: string,
     public avatar: number,
-    public testing = '',
-    public lessonsEn = '',
-    public lessonsRu = '',
-    public typingAdventure = '',
-    public typingHero = '',
-    public badges = ''
+    public testing = Data.noData,
+    public lessonsEn = Data.noData,
+    public lessonsRu = Data.noData,
+    public typingAdventure = Data.noData,
+    public typingHero = Data.noData,
+    public badges = Data.noData
   ) {
     this.login = login;
     this.password = password;
