f955a8214dfd03e24f31756f0f42250d08d3c316
IDemidova <irina.demidova.irina@gmail.com>
2022-08-27 15:03:53 +0300

    fix: add missing fields for user data type and user info class

--- front-end/src/scripts/user/user-info.ts
+++ front-end/src/scripts/user/user-info.ts
@@ -1,10 +1,26 @@
 import { UserData } from '../../types/types';
 
 class UserInfo implements UserData {
-  constructor(public login: string, public password: string, public avatar: number) {
+  constructor(
+    public login: string,
+    public password: string,
+    public avatar: number,
+    public testing = '',
+    public lessonsEn = '',
+    public lessonsRu = '',
+    public typingAdventure = '',
+    public typingHero = '',
+    public badges = ''
+  ) {
     this.login = login;
     this.password = password;
     this.avatar = avatar;
+    this.testing = testing;
+    this.lessonsEn = lessonsEn;
+    this.lessonsRu = lessonsRu;
+    this.typingAdventure = typingAdventure;
+    this.typingHero = typingHero;
+    this.badges = badges;
   }
 }
 
