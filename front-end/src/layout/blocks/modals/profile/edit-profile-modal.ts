061b1a2e3e57755c25a0b2eb17a6f42e00bdeb1d
IDemidova <irina.demidova.irina@gmail.com>
2022-08-27 21:07:43 +0300

    feat: add subtitle for avatars block

--- front-end/src/layout/blocks/modals/profile/edit-profile-modal.ts
+++ front-end/src/layout/blocks/modals/profile/edit-profile-modal.ts
@@ -2,13 +2,15 @@ import State from '../../../../scripts/state/state';
 import CreateElement from '../../../elements/create-element';
 import EditProfileHeader from './edit-profile-header';
 import ProfileData from './profile-data';
+import SaveChangesBtn from './save-changes-btn';
+import ProfileAvatars from './profile-avatars';
+import DeleteAccountBtn from './delete-account-btn';
+import ProfileSubtitle from './profile-subtitle';
+import translation from '../../../../data/translation';
 
 import { Profile, Tag } from '../../../../types/enums';
 import { RenderHandler } from '../../../../types/types';
 import { User } from '../../../../types/interfaces';
-import SaveChangesBtn from './save-changes-btn';
-import ProfileAvatars from './profile-avatars';
-import DeleteAccountBtn from './delete-account-btn';
 
 class EditProfileModal {
   public static createEditProfileModal(user: User, render: RenderHandler): HTMLElement {
@@ -16,10 +18,11 @@ class EditProfileModal {
     const header = EditProfileHeader.createEditProfileHeader(modal);
     const saveChangesBtn = SaveChangesBtn.createSaveChangesBtn();
     const data = ProfileData.createViewProfileData(user, Profile.edit, saveChangesBtn);
+    const avatarsTitle = ProfileSubtitle.createProfileSubtitle(translation.profileAvatarsTitle[State.currentLang]);
     const avatars = ProfileAvatars.createProfileAvatars(data);
     const deleteAccountBtn = DeleteAccountBtn.createDeleteAccountBtn(user);
 
-    modal.append(header, data, avatars, deleteAccountBtn, saveChangesBtn);
+    modal.append(header, data, avatarsTitle, avatars, deleteAccountBtn, saveChangesBtn);
 
     if (State.currentUser.login === '42') {
       render();
