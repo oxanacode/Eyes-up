d1c2a9b4388d7f90ff21273c3e0d6566e8db75a2
IDemidova <irina.demidova.irina@gmail.com>
2022-08-28 12:26:41 +0300

    refactor: resolve merge conflicts

--- back-end/routers/v1.js
+++ back-end/routers/v1.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-
-const users = require('../controllers/v1/users');
-const user = require('../controllers/v1/user');
-const lessons = require('../controllers/v1/lessons');
-const lesson = require('../controllers/v1/lesson');
-
-// Users
-router.get('/v1/users', users.getUsers);
-
-// User
-router.get('/v1/users/:login', user.getUser);
-router.post('/v1/users', user.addUser);
-router.patch('/v1/users/:id', user.updateUser);
-router.delete('/v1/users/:id', user.deleteUser);
-
-// Lessons
-router.get('/v1/lessons', lessons.getLessons);
-
-// Lesson
-router.get('/v1/lessons/:id', lesson.getLesson);
-router.post('/v1/lessons', lesson.addLesson);
-router.patch('/v1/lessons/:id', lesson.updateLesson);
-router.delete('/v1/lessons/:id', lesson.deleteLesson);
-
-=======
 const express = require('express');
 const router = express.Router();
 
