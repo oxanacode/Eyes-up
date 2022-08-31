4a48a943198707d88d8a6b83c5ad9c2902bf3deb
IDemidova <irina.demidova.irina@gmail.com>
2022-08-31 13:10:04 +0300

    feat: add translation for about us page

---
+++ front-end/src/data/about-us-translation.ts
@@ -0,0 +1,114 @@
+import { Translation } from '../types/interfaces';
+
+const aboutUsTranslation: Translation = {
+  greeting: {
+    en: 'Hello everyone!',
+    ru: 'Всем привет!',
+  },
+  project: {
+    en: 'The project was created as a final task of the Rolling Scopes School front-end development course.',
+    ru: 'Проект был создан в рамках финального задания курса front-end разработки Rolling Scopes School.',
+  },
+  featuresText: {
+    en: 'The working process and the full feature list of our application can be found by the ',
+    ru: 'С ходом работ и полным списком особенностей нашего приложения можно ознакомиться по ',
+  },
+  featuresLink: {
+    en: 'link',
+    ru: 'ссылке',
+  },
+  frontEnd: {
+    en: 'Technologies used for front-end: HTML, SCSS, TypeScript.',
+    ru: 'Использованые front-end технологии: HTML, SCSS, TypeScript.',
+  },
+  backEnd: {
+    en: 'Technologies used for back-end: NodeJS, Express, Mongoose, MongoDB, Heroku.',
+    ru: 'Использованые back-end технологии: NodeJS, Express, Mongoose, MongoDB, Heroku.',
+  },
+  nameOxana: {
+    en: 'Oxana',
+    ru: 'Оксана',
+  },
+  taskOneOxana: {
+    en: 'design',
+    ru: 'дизайн',
+  },
+  taskTwoOxana: {
+    en: 'tests/lessons content',
+    ru: 'содержимое тестов/уроков',
+  },
+  taskThreeOxana: {
+    en: 'layout (home, lessons layout choice, list of lessons, test, lesson, games choice)',
+    ru: 'верстка (главная, выбор раскладки для уроков, список уроков, тест, урок, выбор игр)',
+  },
+  taskFourOxana: {
+    en: 'logic (test, lesson, lesson customization)',
+    ru: 'логика (тест, урок, кастомизация урока)',
+  },
+  taskFiveOxana: {
+    en: 'back-end development',
+    ru: 'разработка back-end',
+  },
+  nameNikita: {
+    en: 'Nikita',
+    ru: 'Никита',
+  },
+  taskOneNikita: {
+    en: 'games concepts',
+    ru: 'концепты игр',
+  },
+  taskTwoNikita: {
+    en: 'layout (footer, both games)',
+    ru: 'верстка (подвал, обе игры)',
+  },
+  taskThreeNikita: {
+    en: 'logic (both games)',
+    ru: 'логика (обе игры)',
+  },
+  taskFourNikita: {
+    en: 'back-end deploy',
+    ru: 'развертывание back-end',
+  },
+  nameIrina: {
+    en: 'Irina',
+    ru: 'Ирина',
+  },
+  taskOneIrina: {
+    en: 'layout (header, all modal windows, "About Us" page)',
+    ru: 'верстка (шапка, все модальные окна, страница "О нас")',
+  },
+  taskTwoIrina: {
+    en: 'logic (registration/authorization, data change/logout/profile deletion, display of statistics/badges in a profile)',
+    ru: 'логика (регистрация/авторизация, смена данных/выход/удаление профиля, отображение статистики/достижений в профиле)',
+  },
+  taskThreeIrina: {
+    en: 'work with local storage',
+    ru: 'работа с local storage',
+  },
+  taskFourIrina: {
+    en: 'back-end development',
+    ru: 'разработка back-end',
+  },
+  inspyredBy: {
+    en: 'Inspired by ',
+    ru: 'Идея проекта взята с сайта ',
+  },
+  typingClub: {
+    en: 'TypingClub',
+    ru: 'TypingClub',
+  },
+  imagesBy: {
+    en: 'Illustrations are created by ',
+    ru: 'Иллюстрации созданы ',
+  },
+  illustratorOne: {
+    en: 'Spectrum UI',
+    ru: 'Spectrum UI',
+  },
+  illustratorTwo: {
+    en: 'Alzea Arafat',
+    ru: 'Alzea Arafat',
+  },
+};
+
+export default aboutUsTranslation;
